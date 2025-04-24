5b2ecab2cab3a195db16abe30e6ea373702375c4
Viktoriya Elizaryan <victoriya.elizaryan@tabby.ai>
2025-04-24 09:43:13 +0200

    module-2: fix

--- internal/core/domain/model/kernel/location.go
+++ internal/core/domain/model/kernel/location.go
@@ -1,7 +1,9 @@
 package kernel
 
 import (
+	"delivery/internal/pkg/errs"
 	"errors"
+	"fmt"
 	"math"
 	"math/rand"
 )
@@ -10,14 +12,24 @@ var (
 	ErrorInvalidCoordinate = errors.New("coordinate must be between 1 and 10 inclusive")
 )
 
+const (
+	minX int = 1
+	maxX int = 10
+	minY int = 1
+	maxY int = 10
+)
+
 type Location struct {
 	x int
 	y int
 }
 
 func NewLocation(x, y int) (Location, error) {
-	if x < 1 || x > 10 || y < 1 || y > 10 {
-		return Location{}, ErrorInvalidCoordinate
+	if x < minX || x > maxX {
+		return Location{}, errs.NewValueIsOutOfRangeError("x", x, minX, maxX)
+	}
+	if y < minY || y > maxY {
+		return Location{}, errs.NewValueIsOutOfRangeError("y", y, minY, maxY)
 	}
 	return Location{
 		x: x,
@@ -37,15 +49,35 @@ func (l Location) Equals(other Location) bool {
 	return l.x == other.x && l.y == other.y
 }
 
-func (l Location) DistanceTo(other Location) int {
+func (l Location) DistanceTo(other Location) (int, error) {
+	if err := l.IsValid(); err != nil {
+		return 0, fmt.Errorf("invalid origin location: %w", err)
+	}
+	if err := other.IsValid(); err != nil {
+		return 0, fmt.Errorf("invalid target location: %w", err)
+	}
+
 	dx := int(math.Abs(float64(l.x - other.x)))
 	dy := int(math.Abs(float64(l.y - other.y)))
-	return dx + dy
+	return dx + dy, nil
+}
+
+func (l Location) IsValid() error {
+	if l.x < minX || l.x > maxX {
+		return errs.NewValueIsOutOfRangeError("x", l.x, minX, maxX)
+	}
+	if l.y < minY || l.y > maxY {
+		return errs.NewValueIsOutOfRangeError("y", l.y, minY, maxY)
+	}
+	return nil
 }
 
 func CreateRandom() (Location, error) {
-	x := rand.Intn(10) + 1
-	y := rand.Intn(10) + 1
+	x := rand.Intn(maxX) + 1
+	y := rand.Intn(maxY) + 1
 	location, err := NewLocation(x, y)
+	if err != nil {
+		panic("CreateRandom(): failed to create a valid Location: " + err.Error())
+	}
 	return location, err
 }
