3872f04193e86e5e43e2f55cb5c8a06b831d9223
Viktoriya Elizaryan <victoriya.elizaryan@tabby.ai>
2025-05-05 22:30:10 +0200

    module-5: Dispatch service

--- cmd/composition_root.go
+++ cmd/composition_root.go
@@ -1,9 +1,15 @@
 package cmd
 
+import services "delivery/internal/core/domain/sevices"
+
 type CompositionRoot struct {
 }
 
 func NewCompositionRoot(_ Config) CompositionRoot {
 	app := CompositionRoot{}
 	return app
+}
+
+func (cr *CompositionRoot) NewDispatchService() services.DispatchService {
+	return services.NewDispatchService()
 }
